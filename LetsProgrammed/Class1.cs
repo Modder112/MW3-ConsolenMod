3b5519cf4e57f6a9171cbb9d155dd56f7ae623a8
Modder112 <hiho-jo@web.de>
2018-08-01 18:56:05 +0200

    Upload
    i have upload this files!

---
+++ LetsProgrammed/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InfinityScript;
+
+namespace LetsProgrammed
+{
+    public class LetsProgrammed : BaseScript
+    {
+        List<Entity> AllPlayers = new List<Entity>();
+        public LetsProgrammed()
+            : base()
+        {
+            base.PlayerConnected += connected;
+            base.PlayerDisconnected += disconnected;
+        }
+
+        public void connected(Entity player)
+        {
+            AllPlayers.Add(player);
+            Log.Write(LogLevel.All, "Player " + player.Name.ToString() + "[" + player.GUID.ToString() + "] ist auf dem Server connected!(" + GetPlayerAnzahl() + "/" + Call<int>("getvarint","sv_maxclients") + ")"); 
+        }
+
+        public void disconnected(Entity player)
+        {
+            AllPlayers.Remove(player);
+            Log.Write(LogLevel.All, "Player " + player.Name.ToString() + "[" + player.GUID.ToString() + "] ist vom Server gegangen!(" + GetPlayerAnzahl() + "/" + Call<int>("getvarint", "sv_maxclients") + ")"); 
+        }
+        public int GetPlayerAnzahl()
+        {
+            int i = 0;
+            foreach (Entity player in AllPlayers)
+            {
+                i = i + 1;
+            }
+            return i;
+        }
+
+    }
+}
